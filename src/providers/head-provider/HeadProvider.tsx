26e776cc7ec08ba801e89d9990cd5f73832baf43
Nikita <nikitkavershinin2019@gmail.com>
2024-01-30 15:52:47 +0300

    mail counter

--- src/providers/head-provider/HeadProvider.tsx
+++ src/providers/head-provider/HeadProvider.tsx
@@ -3,39 +3,64 @@ import NextProgressBar from 'nextjs-progressbar'
 import { FC } from 'react'
 
 import Favicons from './Favicons'
-import {IType} from "@/shared/types/option.types";
-import {accessColor} from "@/config/constants";
-
-
+import { IType } from '@/shared/types/option.types'
+import { accessColor } from '@/config/constants'
 
 const HeadProvider: FC<IType> = ({ children }) => {
-    return (
-        <>
-            <NextProgressBar
-                color={accessColor}
-                startPosition={0.3}
-                stopDelayMs={200}
-                height={3}
-            />
-            <Head>
-                <meta charSet="UTF-8" />
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1, maximum-scale=1.0"
-                />
+	return (
+		<>
+			<NextProgressBar
+				color={accessColor}
+				startPosition={0.3}
+				stopDelayMs={200}
+				height={3}
+			/>
+			<Head>
+				{/* <!-- Top.Mail.Ru counter --> */}
+				<script
+					type="text/javascript"
+					dangerouslySetInnerHTML={{
+						__html: `
+              var _tmr = window._tmr || (window._tmr = []);
+              _tmr.push({id: "3478248", type: "pageView", start: (new Date()).getTime()});
+              (function (d, w, id) {
+                if (d.getElementById(id)) return;
+                var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
+                ts.src = "https://top-fwz1.mail.ru/js/code.js";
+                var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
+                if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
+              })(document, window, "tmr-code");
+            `,
+					}}
+				/>
+				<noscript>
+					<div>
+						<img
+							src="https://top-fwz1.mail.ru/counter?id=3478248;js=na"
+							style={{ position: 'absolute', left: '-9999px' }}
+							alt="Top.Mail.Ru"
+						/>
+					</div>
+				</noscript>
+				{/* <!-- /Top.Mail.Ru counter --> */}
+				<meta charSet="UTF-8" />
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1, maximum-scale=1.0"
+				/>
 
-                <Favicons />
+				<Favicons />
 
-                <meta name="theme-color" content={'#181B1E'} />
-                <meta name="msapplication-navbutton-color" content={'#181B1E'} />
-                <meta
-                    name="apple-mobile-web-app-status-bar-style"
-                    content={'#181B1E'}
-                />
-            </Head>
-            {children}
-        </>
-    )
+				<meta name="theme-color" content={'#181B1E'} />
+				<meta name="msapplication-navbutton-color" content={'#181B1E'} />
+				<meta
+					name="apple-mobile-web-app-status-bar-style"
+					content={'#181B1E'}
+				/>
+			</Head>
+			{children}
+		</>
+	)
 }
 
 export default HeadProvider
