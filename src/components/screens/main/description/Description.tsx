8ecfef591cd107ae8a5f9d78ea0ac828f791603e
Pavel293 <64923231+Pavel293@users.noreply.github.com>
2024-05-10 18:27:17 +0300

    first blocks

--- src/components/screens/main/description/Description.tsx
+++ src/components/screens/main/description/Description.tsx
@@ -4,6 +4,27 @@ import cn from 'classnames'
 import Image from 'next/image'
 import Description0 from '../../../../assets/icons/Description0.png'
 import { EIcons, Icon as IconInstance } from '../../../../assets/icons/icon'
+import img1 from '../../../../assets/icons/main/png/img1.png'
+import img2 from '../../../../assets/icons/main/png/img2.png'
+import img3 from '../../../../assets/icons/main/png/img3.png'
+import img4 from '../../../../assets/icons/main/png/img4.png'
+import img5 from '../../../../assets/icons/main/png/img5.png'
+import img6 from '../../../../assets/icons/main/png/img6.png'
+import img7 from '../../../../assets/icons/main/png/img7.png'
+import img8 from '../../../../assets/icons/main/png/img8.png'
+import img9 from '../../../../assets/icons/main/png/img9.png'
+import img10 from '../../../../assets/icons/main/png/img10.png'
+import img11 from '../../../../assets/icons/main/png/img11.png'
+import img12 from '../../../../assets/icons/main/png/img12.png'
+import img13 from '../../../../assets/icons/main/png/img13.png'
+import img14 from '../../../../assets/icons/main/png/img14.png'
+import img15 from '../../../../assets/icons/main/png/img15.png'
+import img16 from '../../../../assets/icons/main/png/img16.png'
+import img17 from '../../../../assets/icons/main/png/img17.png'
+import img18 from '../../../../assets/icons/main/png/img18.png'
+import img19 from '../../../../assets/icons/main/png/img19.png'
+import img20 from '../../../../assets/icons/main/png/img20.png'
+import img21 from '../../../../assets/icons/main/png/img21.png'
 const Description: FC = () => {
 	return (
 		<div className={cn(styles.fuck, 'wrapper')}>
@@ -24,100 +45,101 @@ const Description: FC = () => {
 					<div className={styles.box}>
 						<div className={styles.row}>
 							<div className={styles.column}>
-								<IconInstance name={EIcons.img1} />
-								<IconInstance name={EIcons.img2} />
-								<IconInstance name={EIcons.img3} />
-								<IconInstance name={EIcons.img4} />
-								<IconInstance name={EIcons.img5} />
-								<IconInstance name={EIcons.img6} />
-								<IconInstance name={EIcons.img7} />
-								<IconInstance name={EIcons.img1} />
-								<IconInstance name={EIcons.img2} />
-								<IconInstance name={EIcons.img3} />
-								<IconInstance name={EIcons.img4} />
-								<IconInstance name={EIcons.img5} />
-								<IconInstance name={EIcons.img6} />
-								<IconInstance name={EIcons.img7} />
+								<Image src={img1} alt={''} />
+								<Image src={img2} alt={''} />
+								<Image src={img3} alt={''} />
+								<Image src={img4} alt={''} />
+								<Image src={img5} alt={''} />
+								<Image src={img6} alt={''} />
+								<Image src={img7} alt={''} />
+								<Image src={img1} alt={''} />
+								<Image src={img2} alt={''} />
+								<Image src={img3} alt={''} />
+								<Image src={img4} alt={''} />
+								<Image src={img5} alt={''} />
+								<Image src={img6} alt={''} />
+								<Image src={img7} alt={''} />
 							</div>
 							<div className={styles.column}>
-								<IconInstance name={EIcons.img8} />
-								<IconInstance name={EIcons.img9} />
-								<IconInstance name={EIcons.img10} />
-								<IconInstance name={EIcons.img11} />
-								<IconInstance name={EIcons.img12} />
-								<IconInstance name={EIcons.img13} />
-								<IconInstance name={EIcons.img14} />
-								<IconInstance name={EIcons.img8} />
-								<IconInstance name={EIcons.img9} />
-								<IconInstance name={EIcons.img10} />
-								<IconInstance name={EIcons.img11} />
-								<IconInstance name={EIcons.img12} />
-								<IconInstance name={EIcons.img13} />
-								<IconInstance name={EIcons.img14} />
+								<Image src={img8} alt={''} />
+								<Image src={img9} alt={''} />
+								<Image src={img10} alt={''} />
+								<Image src={img11} alt={''} />
+								<Image src={img12} alt={''} />
+								<Image src={img13} alt={''} />
+								<Image src={img14} alt={''} />
+								<Image src={img8} alt={''} />
+								<Image src={img9} alt={''} />
+								<Image src={img10} alt={''} />
+								<Image src={img11} alt={''} />
+								<Image src={img12} alt={''} />
+								<Image src={img13} alt={''} />
+								<Image src={img14} alt={''} />
 							</div>
 							<div className={styles.column}>
-								<IconInstance name={EIcons.img15} />
-								<IconInstance name={EIcons.img16} />
-								<IconInstance name={EIcons.img17} />
-								<IconInstance name={EIcons.img18} />
-								<IconInstance name={EIcons.img19} />
-								<IconInstance name={EIcons.img20} />
-								<IconInstance name={EIcons.img21} />
-								<IconInstance name={EIcons.img15} />
-								<IconInstance name={EIcons.img16} />
-								<IconInstance name={EIcons.img17} />
-								<IconInstance name={EIcons.img18} />
-								<IconInstance name={EIcons.img19} />
-								<IconInstance name={EIcons.img20} />
-								<IconInstance name={EIcons.img21} />
+								<Image src={img15} alt={''} />
+								<Image src={img16} alt={''} />
+								<Image src={img17} alt={''} />
+								<Image src={img18} alt={''} />
+								<Image src={img19} alt={''} />
+								<Image src={img20} alt={''} />
+								<Image src={img21} alt={''} />
+								<Image src={img15} alt={''} />
+								<Image src={img16} alt={''} />
+								<Image src={img17} alt={''} />
+								<Image src={img18} alt={''} />
+								<Image src={img19} alt={''} />
+								<Image src={img20} alt={''} />
+								<Image src={img21} alt={''} />
+							</div>
+
+							<div className={styles.column}>
+								<Image src={img1} alt={''} />
+								<Image src={img2} alt={''} />
+								<Image src={img3} alt={''} />
+								<Image src={img4} alt={''} />
+								<Image src={img5} alt={''} />
+								<Image src={img6} alt={''} />
+								<Image src={img7} alt={''} />
+								<Image src={img1} alt={''} />
+								<Image src={img2} alt={''} />
+								<Image src={img3} alt={''} />
+								<Image src={img4} alt={''} />
+								<Image src={img5} alt={''} />
+								<Image src={img6} alt={''} />
+								<Image src={img7} alt={''} />
 							</div>
 							<div className={styles.column}>
-								<IconInstance name={EIcons.img22} />
-								<IconInstance name={EIcons.img23} />
-								<IconInstance name={EIcons.img24} />
-								<IconInstance name={EIcons.img25} />
-								<IconInstance name={EIcons.img26} />
-								<IconInstance name={EIcons.img27} />
-								<IconInstance name={EIcons.img28} />
-								<IconInstance name={EIcons.img22} />
-								<IconInstance name={EIcons.img23} />
-								<IconInstance name={EIcons.img24} />
-								<IconInstance name={EIcons.img25} />
-								<IconInstance name={EIcons.img26} />
-								<IconInstance name={EIcons.img27} />
-								<IconInstance name={EIcons.img28} />
+								<Image src={img8} alt={''} />
+								<Image src={img9} alt={''} />
+								<Image src={img10} alt={''} />
+								<Image src={img11} alt={''} />
+								<Image src={img12} alt={''} />
+								<Image src={img13} alt={''} />
+								<Image src={img14} alt={''} />
+								<Image src={img8} alt={''} />
+								<Image src={img9} alt={''} />
+								<Image src={img10} alt={''} />
+								<Image src={img11} alt={''} />
+								<Image src={img12} alt={''} />
+								<Image src={img13} alt={''} />
+								<Image src={img14} alt={''} />
 							</div>
 							<div className={styles.column}>
-								<IconInstance name={EIcons.img29} />
-								<IconInstance name={EIcons.img30} />
-								<IconInstance name={EIcons.img31} />
-								<IconInstance name={EIcons.img32} />
-								<IconInstance name={EIcons.img33} />
-								<IconInstance name={EIcons.img34} />
-								<IconInstance name={EIcons.img35} />
-								<IconInstance name={EIcons.img29} />
-								<IconInstance name={EIcons.img30} />
-								<IconInstance name={EIcons.img31} />
-								<IconInstance name={EIcons.img32} />
-								<IconInstance name={EIcons.img33} />
-								<IconInstance name={EIcons.img34} />
-								<IconInstance name={EIcons.img35} />
-							</div>
-							<div className={styles.column}>
-								<IconInstance name={EIcons.img36} />
-								<IconInstance name={EIcons.img37} />
-								<IconInstance name={EIcons.img38} />
-								<IconInstance name={EIcons.img39} />
-								<IconInstance name={EIcons.img40} />
-								<IconInstance name={EIcons.img41} />
-								<IconInstance name={EIcons.img42} />
-								<IconInstance name={EIcons.img36} />
-								<IconInstance name={EIcons.img37} />
-								<IconInstance name={EIcons.img38} />
-								<IconInstance name={EIcons.img39} />
-								<IconInstance name={EIcons.img40} />
-								<IconInstance name={EIcons.img41} />
-								<IconInstance name={EIcons.img42} />
+								<Image src={img15} alt={''} />
+								<Image src={img16} alt={''} />
+								<Image src={img17} alt={''} />
+								<Image src={img18} alt={''} />
+								<Image src={img19} alt={''} />
+								<Image src={img20} alt={''} />
+								<Image src={img21} alt={''} />
+								<Image src={img15} alt={''} />
+								<Image src={img16} alt={''} />
+								<Image src={img17} alt={''} />
+								<Image src={img18} alt={''} />
+								<Image src={img19} alt={''} />
+								<Image src={img20} alt={''} />
+								<Image src={img21} alt={''} />
 							</div>
 						</div>
 					</div>
--- src/providers/head-provider/HeadProvider.tsx
+++ src/providers/head-provider/HeadProvider.tsx
@@ -17,64 +17,6 @@ const HeadProvider: FC<IType> = ({ children }) => {
 			/>
 
 			<Head>
-				{/* <!-- Top.Mail.Ru counter --> */}
-				<noscript>6zk0syi22z79zb81</noscript>
-				<script
-					type="text/javascript"
-					dangerouslySetInnerHTML={{
-						__html: `
-              var _tmr = window._tmr || (window._tmr = []);
-              _tmr.push({id: "3478248", type: "pageView", start: (new Date()).getTime()});
-              (function (d, w, id) {
-                if (d.getElementById(id)) return;
-                var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
-                ts.src = "https://top-fwz1.mail.ru/js/code.js";
-                var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
-                if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
-              })(document, window, "tmr-code");
-            `,
-					}}
-				/>
-				<noscript>
-					<div>
-						<img
-							src="https://top-fwz1.mail.ru/counter?id=3478248;js=na"
-							style={{ position: 'absolute', left: '-9999px' }}
-							alt="Top.Mail.Ru"
-						/>
-					</div>
-				</noscript>
-				{/* <!-- /Top.Mail.Ru counter --> */}
-				{/*<!-- Yandex.Metrika counter -->*/}
-				<script
-					type="text/javascript"
-					dangerouslySetInnerHTML={{
-						__html: `
-					(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-					m[i].l=1*new Date();
-					for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-					k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-					(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-					ym(96922814, "init", {
-					clickmap:true,
-					trackLinks:true,
-					accurateTrackBounce:true,
-					webvisor:true
-				});
-        `,
-					}}
-				/>
-				<noscript>
-					<div>
-						<img
-							src="https://mc.yandex.ru/watch/96922814"
-							style={{ position: 'absolute', left: '-9999px' }}
-							alt=""
-						/>
-					</div>
-				</noscript>
-				{/*<!-- Yandex.Metrika counter -->*/}
 				<meta charSet="UTF-8" />
 				<meta
 					name="viewport"
